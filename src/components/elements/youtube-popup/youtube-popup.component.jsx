0f3627a48798b539ec9dccdc6962fe875003c3a9
DanielYorwarthAthlon <daniel.yorwarth@weareathlon.com>
2022-02-13 15:55:55 +0000

    fix: added conditional check for server sider rendering to try and stop modal failing build

--- src/components/elements/youtube-popup/youtube-popup.component.jsx
+++ src/components/elements/youtube-popup/youtube-popup.component.jsx
@@ -1,15 +1,20 @@
 import React,{useState} from 'react';
-import ModalVideo from 'react-modal-video';
 import Img from 'gatsby-image';
+import ModalVideo from 'react-modal-video'
 
 import play from '../../../images/Icons/play.svg';
 
+
 const YoutubePopUp = ({videoID, image}) => {
     const [isOpen, setOpen] = useState(false)
+    let modal = <></>
+    if (typeof(window) !== 'undefined') {
+      modal = <ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId={videoID} onClose={() => setOpen(false)} />
+    }
 
     return (
         <div>
-            <ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId={videoID} onClose={() => setOpen(false)} />
+            {modal}
             <div className="mb-5 md:mb-10 relative cursor-pointer duration-300 transform hover:-translate-y-4 hover:shadow-2xl" onClick={()=> setOpen(true)}>
                 <Img fluid={image} />
                 <img src={play} alt="Play Icon" className="absolute left-2/4 top-2/4 transform -translate-y-2/4 -translate-x-2/4"/>
