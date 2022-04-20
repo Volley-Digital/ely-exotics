f8ffe5bbf436ca96067b084469fe39f01247bee4
DanielYorwarthAthlon <daniel.yorwarth@weareathlon.com>
2022-04-20 14:36:53 +0100

    fix: form required fields and added correct post data to netlify

--- src/components/elements/form/form.component.jsx
+++ src/components/elements/form/form.component.jsx
@@ -26,7 +26,7 @@ const Form = () => {
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: encode({ 
           "form-name": event.target.getAttribute("name"), 
-          ...name
+          ...inputs
         })
       })
       // On success, redirect to the custom success page using Gatsby's `navigate` helper function
@@ -40,19 +40,19 @@ const Form = () => {
             <input type="hidden" name="form-name" value="contact-form" />
             <div className="flex flex-wrap px-4 md:px-5">
                 <div className="w-full md:w-1/2 px-2 md:px-3 mb-4 md:mb-6">
-                    <input onChange={handleInputChange} value={inputs.name} className="w-full bg-white-0 px-4 py-3 outline-none" type="text" name="name" placeholder="name" />
+                    <input onChange={handleInputChange} value={inputs.name} className="w-full bg-white-0 px-4 py-3 outline-none" required type="text" name="name" placeholder="name" />
                 </div>
                 <div className="w-full md:w-1/2 px-2 md:px-3 mb-4 md:mb-6">
-                    <input onChange={handleInputChange} value={inputs.email} className="w-full bg-white-0 px-4 py-3 outline-none" type="emal" name="emal" placeholder="emal" />
+                    <input onChange={handleInputChange} value={inputs.email} className="w-full bg-white-0 px-4 py-3 outline-none" required type="email" name="email" placeholder="email" />
                 </div>
                 <div className="w-full md:w-1/2 px-2 md:px-3 mb-4 md:mb-6">
-                    <input onChange={handleInputChange} value={inputs.subject} className="w-full bg-white-0 px-4 py-3 outline-none" type="text" name="subject" placeholder="subject" />
+                    <input onChange={handleInputChange} value={inputs.subject} className="w-full bg-white-0 px-4 py-3 outline-none" required type="text" name="subject" placeholder="subject" />
                 </div>
                 <div className="w-full md:w-1/2 px-2 md:px-3 mb-4 md:mb-6">
                     <input onChange={handleInputChange} value={inputs.phone} className="w-full bg-white-0 px-4 py-3 outline-none" type="tel" name="phone" placeholder="phone" />
                 </div>
                 <div className="w-full px-2 md:px-3 mb-4 md:mb-6">
-                    <textarea onChange={handleInputChange} value={inputs.message} className="w-full bg-white-0 px-4 py-3 outline-none" type="text" name="message" rows="8" placeholder="additional information" />
+                    <textarea onChange={handleInputChange} value={inputs.message} className="w-full bg-white-0 px-4 py-3 outline-none" required type="text" name="message" rows="8" placeholder="additional information" />
                 </div>
             </div>
             <button type="submit" className="mx-6 md:mx-8 duration-300 transform hover:-translate-y-2 text-center bg-gradient-to-b from-primary-light to-primary-dark heading-font font-black text-center px-6 py-2 text-lg text-white-0 inline-block">
